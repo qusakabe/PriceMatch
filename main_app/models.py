60f456846ae2aaf694f63d7ac147b158f04a9e4d
Your Name <you@example.com>
2025-03-18 15:54:59 +0300

    init database

--- main_app/models.py
+++ main_app/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+class Shop(models.Model):
+    name = models.CharField(max_length=100)
+    number_products = models.IntegerField()
+
+
+class Product(models.Model):
+    name = models.CharField(max_length=100)
+    price = models.FloatField()
+    image = models.URLField()
+    weight = models.CharField(max_length=20)
+    shop = models.ForeignKey(Shop, on_delete=models.CASCADE)
