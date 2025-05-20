7eb04198bc39f0adbf9698eb60f843dcbde11e93
Роман <105128288+qusakabe@users.noreply.github.com>
2025-05-21 02:00:56 +0300

    Update settings.py

--- PriceMatch/settings.py
+++ PriceMatch/settings.py
@@ -120,7 +120,14 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+
+if DEBUG:
+    STATICFILES_DIRS = [
+        BASE_DIR / "static",
+    ]
+
+DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
