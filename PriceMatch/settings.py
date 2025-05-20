dffb6edde760c8a09ec22bdc8e66edcc8c86465d
Роман <105128288+qusakabe@users.noreply.github.com>
2025-05-21 02:38:21 +0300

    Update settings.py

--- PriceMatch/settings.py
+++ PriceMatch/settings.py
@@ -78,7 +78,7 @@ WSGI_APPLICATION = 'PriceMatch.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': "PriceMatch",
+        'NAME': "postgres",
         'USER': "root",
         'PASSWORD': 12345,
         'HOST': "localhost",
